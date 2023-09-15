0c104d2260937e9874fa519ea691badc50d986b4
Emmanuel Leiva <105935801+geleivalencia@users.noreply.github.com>
2023-09-14 21:02:25 -0600

    ...

--- proyecto6/src/app/components/crear-tarjeta/crear-tarjeta.component.ts
+++ proyecto6/src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { TarjetaService } from 'src/app/services/tarjeta.service';
 
 @Component({
   selector: 'app-crear-tarjeta',
@@ -7,8 +9,23 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./crear-tarjeta.component.css']
 })
 export class CrearTarjetaComponent {
-  //form: FormGroup;
+  form: FormGroup;
   cargando = false;
   titulo = "Agregar tarjeta de crédito";
   id:string | undefined;
+
+  constructor(private fb: FormBuilder,
+    private tarjetaService: TarjetaService,
+    /*private toastr: ToastrService*/){
+      this.form = this.fb.group({
+        titular: ['', Validators.required],
+        numeroTarjeta: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+        fechaExpiracion: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+        cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]]
+      })
+  }
+
+  crearTarjeta(){
+
+  }
 }
